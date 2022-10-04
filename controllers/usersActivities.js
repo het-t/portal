2ca8c2b594b518bb31de76bfd2b5c2a3463623a4
het-t <hettarkhala068@gmail.com>
2022-10-04 17:25:43 +0530

    feature:pagination in userActivities

--- controllers/usersActivities.js
+++ controllers/usersActivities.js
@@ -1,25 +1,12 @@
 import {activityDb} from '../db/activityDb.js'
 
 const usersActivities = (req, res, next) => {
-    // req.log_details = {
-    //     "activity_id": 12,
-    //     "user": req.email,
-    //     "reference_table": "user_activities",
-    //     "reference_table_pk_id": null,
-    // }
     activityDb(req.query.from, req.query.records_per_page)
     .then((activities) => {
-        // console.log(activities)
-        // req.res_data = activities
-        // req.log_details.detail = 'success'
-        // next()
         res.send(activities)
     })
     .catch((err) => {
         res.send("failed")
-        // req.res_data = 'failed'
-        // req.log_details.detail = [err]
-        // next()
     })
 }
 
