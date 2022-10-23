70fbec93a9093501b6f7eb9f7381a44a4b3b1ee5
het-t <hettarkhala068@gmail.com>
2022-10-23 22:24:02 +0530

    s

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 import express from 'express'
 import cookieParser from 'cookie-parser'
-
+import path from 'path'
+import {fileURLToPath} from 'url'
 import router from './routes/index.js'
 
 const app = express()
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+app.use(express.static(path.join(__dirname, 'dist')))
 
 app.use(cookieParser('secret'))
 app.use(express.urlencoded({extended: true}))
