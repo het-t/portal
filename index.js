9baaa4127006cf1cfa0f0634edb4d9339e4871fb
het-t <hettarkhala068@gmail.com>
2022-10-23 22:14:28 +0530

    f

--- index.js
+++ index.js
@@ -2,12 +2,18 @@ import express from 'express'
 import cookieParser from 'cookie-parser'
 import {indexRoutes} from './routes/indexRoutes.js'
 import {userRoutes} from './routes/userRoutes.js'
+import path from 'path'
+import {fileURLToPath} from 'url'
 
 const app = express()
 
 app.use(cookieParser('secret'))
 app.use(express.urlencoded({extended: true}))
 app.use(express.json({limit:'5mb'}))
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+app.use(express.static(path.join(__dirname, 'dist')))
 
 app.use('/u/api', userRoutes)
 app.use('/api', indexRoutes)
