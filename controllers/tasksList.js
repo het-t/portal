ab4dc865beed02d69df7e1bc3aa6c0f3d630ce44
het-t <hettarkhala068@gmail.com>
2022-11-14 16:07:36 +0530

    feature: get tasks

---
+++ controllers/tasksList.js
@@ -0,0 +1,23 @@
+import makeDbReq from '../db/index.js'
+
+const getTasks = (req, res, next) => {
+    req.log_details = {
+        "activity_id": 20,
+        "user": req.email,
+        "reference_table": "tasks_master",
+        "reference_table_pk_id": null,
+    }
+    makeDbReq(`tasks_master_get_tasks()`, [])
+    .then((tasks) => {
+        req.log_details.detail = 'success'
+        req.res_data = tasks
+        next()
+    })
+    .catch((err) => {
+        req.log_details.detail = [err]
+        req.res_data = err
+        next()
+    })
+}
+
+export default getTasks
