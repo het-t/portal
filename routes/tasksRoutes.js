14de165e4b66cdb42877c3ca97fada9bf21ea643
het-t <hettarkhala068@gmail.com>
2022-11-14 16:07:57 +0530

    fix: removed unnecessry route

--- routes/tasksRoutes.js
+++ routes/tasksRoutes.js
@@ -7,10 +7,11 @@ import createSubTasks from '../controllers/taskSubTasksCreate.js'
 import auth from '../controllers/auth.js'
 import addLog from '../controllers/logs.js'
 import getSubTasks from '../controllers/subTasksList.js'
+import getTasks from '../controllers/tasksList.js'
 
 const router = express.Router()
 
-router.get('/', auth, getAllUsers, addLog)
+router.get('/', auth, getTasks, addLog)
 router.get('/create-task', auth, createTask, createSubTasks, addLog)
 router.get('/get-sub-tasks', auth, getSubTasks, addLog)
 
