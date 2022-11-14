ae1f4ec787f0ead42adff59caf465e4835f7a2ba
het-t <hettarkhala068@gmail.com>
2022-11-14 15:38:54 +0530

    feature: get sub tasks

--- routes/tasksRoutes.js
+++ routes/tasksRoutes.js
@@ -6,10 +6,12 @@ import createSubTasks from '../controllers/taskSubTasksCreate.js'
 
 import auth from '../controllers/auth.js'
 import addLog from '../controllers/logs.js'
+import getSubTasks from '../controllers/subTasksList.js'
 
 const router = express.Router()
 
 router.get('/', auth, getAllUsers, addLog)
 router.get('/create-task', auth, createTask, createSubTasks, addLog)
+router.get('/get-sub-tasks', auth, getSubTasks, addLog)
 
 export default router
