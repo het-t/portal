fa3c26b5dd4c2975b749f340ecf6b47abe19f229
het-t <hettarkhala068@gmail.com>
2023-01-02 18:13:27 +0530

    fix

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 import express from 'express'
 import cookieParser from 'cookie-parser'
-import path from 'path'
-import {fileURLToPath} from 'url'
+// import path from 'path'
+// import {fileURLToPath} from 'url'
 import router from './routes/index.js'
+
+import https from 'https'
+import fs from 'fs'
+
+var privateKey  = fs.readFileSync('./server.key', 'utf8');
+var certificate = fs.readFileSync('./server.cert', 'utf8');
+var credentials = {key: privateKey, cert: certificate}
 
 const app = express()
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
-app.use(express.static(path.join(__dirname, 'dist')))
+var httpsServer = https.createServer(credentials, app)
+
+// const __filename = fileURLToPath(import.meta.url)
+// const __dirname = path.dirname(__filename)
+// app.use(express.static(path.join(__dirname, 'dist')))
 
 app.use(cookieParser('secret'))
 app.use(express.urlencoded({extended: true}))
@@ -18,6 +27,6 @@ app.use(express.json({limit:'5mb'}))
 app.use('/u/api', router)
 app.use('/api', router)
 
-app.listen(8181, ()=>{
+httpsServer.listen(8181, ()=>{
     console.log("PORT 8181")
 })
