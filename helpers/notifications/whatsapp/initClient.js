aa71578adf5627d09d940a130b38a3da8ddf14f1
het-t <hettarkhala068@gmail.com>
2023-05-12 01:18:14 +0530

    bug solved

--- helpers/notifications/whatsapp/initClient.js
+++ helpers/notifications/whatsapp/initClient.js
@@ -17,7 +17,7 @@ export function initClient(orgId) {
     
         if (clientId) {
             client.on('qr', () => {
-                destroyClient.call(call)
+                destroyClient.call(client)
                     
                 
                 reject(`${orgId} NOT_AUTHORISED`)
@@ -27,7 +27,7 @@ export function initClient(orgId) {
         
             client.on('auth_failure', () => {
                 
-                destroyClient.call(call)
+                destroyClient.call(client)
                     
                               
                 reject(`${orgId} AUTH_FAILURE`)
@@ -41,7 +41,7 @@ export function initClient(orgId) {
         
             client.initialize()
             .catch(err => {
-                destroyClient.call(call)
+                destroyClient.call(client)
                 console.error(err)
             })
         }
