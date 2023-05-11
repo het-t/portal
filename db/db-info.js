d37a709cf430bbde09608abc379c73013af27a20
het-t <hettarkhala068@gmail.com>
2023-05-11 08:59:04 +0530

    changes specific for deployment

---
+++ db/db-info.js
@@ -0,0 +1,10 @@
+const dbInfo = {
+    "host": "database-1.ckafoymncy5f.eu-north-1.rds.amazonaws.com",
+    "user": "admin",
+    // "host": "localhost",
+    // "user": "root",
+    "password": "het161967",
+    "database": "portal",
+    "multipleStatements": true
+}
+export default dbInfo
---
+++ index.js
@@ -0,0 +1,38 @@
+import * as dotenv from 'dotenv'
+import express from 'express'
+import cookieParser from 'cookie-parser'
+import router from './routes/index.js'
+import {getClients} from './helpers/notifications/whatsapp/initClient.js'
+import notificationWaSend from './helpers/notifications/whatsapp/notificationsWaSend.js'
+import {join} from 'path'
+import * as url from 'url';
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+
+const app = express()
+
+dotenv.config()
+
+app.use(cookieParser('secret'))
+app.use(express.urlencoded({extended: true}))
+app.use(express.json({limit:'10mb'}))
+
+app.use(express.static(join(__dirname, './dist')))
+
+app.use('/u/api', router)
+app.use('/api', router)
+
+app.listen(process.env.PORT, () => {
+    // notificationWaSend()
+    setInterval(notificationWaSend, 1000*30)
+    console.log(`PORT ${process.env.PORT}`)
+})
+
+process.on('SIGINT', () => {
+    let clients = getClients()
+
+    Object.values(clients).forEach((client) => {
+        return client.destroy()
+    })
+
+    process.exit(0)
+})
