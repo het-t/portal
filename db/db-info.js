ceb648092236a57ced38c152bc530fbbc6a3de7e
het-t <hettarkhala068@gmail.com>
2023-05-12 00:50:21 +0530

    local database created

--- db/db-info.js
+++ db/db-info.js
@@ -1,8 +1,8 @@
 const dbInfo = {
-    "host": "database-1.ckafoymncy5f.eu-north-1.rds.amazonaws.com",
-    "user": "admin",
-    // "host": "localhost",
-    // "user": "root",
+    // "host": "database-1.ckafoymncy5f.eu-north-1.rds.amazonaws.com",
+    // "user": "admin",
+    "host": "localhost",
+    "user": "root",
     "password": "het161967",
     "database": "portal",
     "multipleStatements": true
